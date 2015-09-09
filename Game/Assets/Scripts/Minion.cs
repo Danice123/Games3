0f7e21f77d5b37f640aa015e16064491ca4bba14
Daniel Mitchell <mitchelldanice@gmail.com>
2015-09-09 02:16:39 -0400

    Minions

---
+++ Game/Assets/Scripts/Minion.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Minion : MonoBehaviour {
+
+	public bool isLeftMinion = true;
+	public float moveSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate() {
+		Vector2 vel = GetComponent<Rigidbody2D> ().velocity;
+
+		if (isLeftMinion)
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveSpeed, vel.y);
+		else
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (-moveSpeed, vel.y);
+	}
+}
---
+++ Game/Assets/Scripts/Spawn.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+
+	public GameObject spawn;
+	private int timer = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+		if (timer == 600) {
+			Vector3 pos = GetComponent<Transform>().position;
+			Instantiate(spawn, new Vector3 (pos.x, pos.y, pos.z), Quaternion.identity);
+			timer = 0;
+		}
+		timer++;
+	}
+}
