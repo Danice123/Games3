3ed3103933a66255b4b5d0a9c967c2e75a5248df
curtismj1 <curtismj1@gcc.edu>
2015-09-16 21:01:55 -0400

    Bare-bones menu
    Unity is killin me. We should get together tomorrow and someone can
    teach me how to do basic things. Anyway, here's Wonderwall.

---
+++ Game/Assets/menuScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	public void LoadScene(int level)
+    {
+        Application.LoadLevel(level);
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
