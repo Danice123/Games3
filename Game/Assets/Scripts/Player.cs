424930b584b32593e14a8940f0886ed1ac500cda
curtismj1 <curtismj1@gcc.edu>
2015-09-17 21:28:26 -0400

    Made stuff happen

--- Game/Assets/Scripts/Player.cs
+++ Game/Assets/Scripts/Player.cs
@@ -15,13 +15,17 @@ public class Player : MonoBehaviour {
 	public int largeArrowSpeed = 20;
 
 	private Vector2 facing = new Vector2(1, 0);
-
-	// Use this for initialization
-	void Start () {
+    private Vector2 prevFacing = new Vector2(1, 0);
+    // Use this for initialization
+    void Start () {
         samurai = GameObject.Find("samurai");
-        samurai.GetComponent<Animation>().Play("Idle");
+        samurai.GetComponent<Animation>()["Attack"].layer = 3;
+        samurai.GetComponent<Animation>()["Jump"].layer = 2;
         samurai.GetComponent<Animation>()["Attack"].wrapMode = WrapMode.Once;
+        samurai.GetComponent<Animation>()["Jump"].wrapMode = WrapMode.Once;
         samurai.GetComponent<Animation>()["Attack"].speed = 2.0f;
+        samurai.GetComponent<Animation>()["Run"].layer = 1;
+        samurai.GetComponent<Animation>()["Run"].wrapMode = WrapMode.Once;
     }
 	
 	// Update is called once per frame
@@ -30,10 +34,20 @@ public class Player : MonoBehaviour {
 	}
 
 	void FixedUpdate() {
+        bool buttonPressed = false;
 		if (Input.GetAxisRaw ("Horizontal") != 0 || Input.GetAxisRaw ("Vertical") != 0) {
 			facing = new Vector2(Input.GetAxisRaw("Horizontal"), -Input.GetAxisRaw("Vertical")).normalized;
             
-		}
+            samurai.GetComponent<Animation>().Play("Run");
+
+            if (facing.x > 0.0f && prevFacing.x < 0.0f || facing.x < 0.0f && prevFacing.x > 0.0f)
+            {
+                samurai.transform.Rotate(0, 180, 0);
+                
+            }
+            prevFacing = facing;
+           
+        }
 
 		Vector2 vel = GetComponent<Rigidbody2D> ().velocity;
 
@@ -42,7 +56,9 @@ public class Player : MonoBehaviour {
 		if (Input.GetButtonDown ("A") && jumpTimes > 0) {
 			GetComponent<Rigidbody2D> ().velocity = new Vector2 (ha, jumpSpeed);
 			jumpTimes--;
-		} else {
+            samurai.GetComponent<Animation>().Play("Jump");
+            buttonPressed = true;
+        } else {
 			GetComponent<Rigidbody2D> ().velocity = new Vector2 (ha, vel.y);
 		}
 
@@ -51,13 +67,18 @@ public class Player : MonoBehaviour {
 			a.GetComponent<Rigidbody2D>().velocity = facing * arrowSpeed + new Vector2(0, 5);
 			a.tag = gameObject.tag;
             samurai.GetComponent<Animation>().Play("Attack");
+            buttonPressed = true;
         }
 
 		if (Input.GetButtonDown ("Y")) {
 			GameObject a = (GameObject) Instantiate(largeArrow, GetComponent<Transform>().position, Quaternion.identity);
 			a.GetComponent<Rigidbody2D>().velocity = facing * largeArrowSpeed;
 			a.tag = gameObject.tag;
-		}
+            buttonPressed = true;
+        }
+     
+            samurai.GetComponent<Animation>().Play("idle");
+        
 	}
 
 	void OnCollisionEnter2D (Collision2D hit) {
