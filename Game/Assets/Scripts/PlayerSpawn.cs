54963413b11f38fd69c4fe0ad78cc58ac7bbd586
curtismj1 <curtismj1@gcc.edu>
2015-09-28 09:04:55 -0400

    mage stuff

--- Game/Assets/Scripts/PlayerSpawn.cs
+++ Game/Assets/Scripts/PlayerSpawn.cs
@@ -5,6 +5,7 @@ public class PlayerSpawn : MonoBehaviour {
 
 	public GameObject ranger;
 	public GameObject warrior;
+    public GameObject mage;
 
 	public static int playerType;
 
@@ -16,9 +17,12 @@ public class PlayerSpawn : MonoBehaviour {
 		}
 		if (playerType == 1) {
 			g = (GameObject) Instantiate(warrior, new Vector3(0, 2), Quaternion.identity);
-
 		}
-		g.tag = "Left";
+        if (playerType == 2)
+        {
+            g = (GameObject)Instantiate(mage, new Vector3(0, 2), Quaternion.identity);
+        }
+        g.tag = "Left";
 		GameObject.Find ("Camera").GetComponent<CameraControl>().tracking = g;
 		GameObject.Find ("Boss").GetComponent<Boss> ().target = g;
 	}
