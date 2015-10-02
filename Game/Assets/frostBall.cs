f1b34ab28811953fcef60424d23bc5379a76dd2c
curtismj1 <curtismj1@gcc.edu>
2015-10-02 07:24:33 -0400

    Did some fixing of the mage. Also, he can't move now. Don't know what happened there. It's a feature.

---
+++ Game/Assets/frostBall.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class frostBall : MonoBehaviour {
+
+    public int damage = 10;
+    public int ticksAlive = 30;
+    public int rotateSpeed = 100;
+    public string owner;
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (ticksAlive == 0)
+            DestroyObject(gameObject);
+        ticksAlive--;
+
+        Vector2 dir = GetComponent<Rigidbody2D>().velocity;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        var q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, q, rotateSpeed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Ground") || collider.CompareTag("shield"))
+        {
+            DestroyObject(gameObject);
+        }
+        if ((collider.CompareTag("Left") || collider.CompareTag("Right")) && !collider.CompareTag(owner))
+        {
+            collider.gameObject.GetComponent<Health>().health -= damage;
+            DestroyObject(gameObject);
+        }
+    }
+}
--- Game/Assets/p3.cs
+++ Game/Assets/p3.cs
@@ -8,9 +8,9 @@ public class p3 : MonoBehaviour
     public float moveSpeed = 1.0f;
     public float jumpSpeed = 1.0f;
     public int respawnTimer = 60;
-
+    public GameObject frostBall;
     public bool canMove = true;
-
+    const float FROSTBALL_SPEED = 15.0f;
     public Vector2 facing = new Vector2(1, 0);
     public int jumped = 0;
 
@@ -38,6 +38,7 @@ public class p3 : MonoBehaviour
 
     void FixedUpdate()
     {
+        string playerNumber = GetComponent<Player>().playerNumber;
         if (GetComponent<Health>().health <= 0)
         {
             respawnTimer = 60 * 1;
@@ -45,6 +46,16 @@ public class p3 : MonoBehaviour
             GetComponent<Health>().health = GetComponent<Health>().maxHealth;
             GameObject.Find("Respawn").GetComponent<Respawn>().addSpawn(gameObject);
             gameObject.SetActive(false);
+        }
+        if (Input.GetButtonDown(playerNumber + "Ability1"))
+        {
+
+            float angle = Mathf.Atan2(facing.y, facing.x) * Mathf.Rad2Deg;
+
+            GameObject a = (GameObject)Instantiate(frostBall, GetComponent<Transform>().position + new Vector3(0, 1, 0), Quaternion.AngleAxis(angle, Vector3.forward));
+            a.GetComponent<Rigidbody2D>().velocity = facing * FROSTBALL_SPEED + new Vector2(0, 5);
+            a.GetComponent<Arrow>().owner = gameObject.tag;
+            GetComponentInChildren<Animator>().SetTrigger("Attack");
         }
 
         if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
