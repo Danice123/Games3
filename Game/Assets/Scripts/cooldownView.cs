8ef78573b7d7565d27d6eedabe161edd8192b698
curtismj1 <curtismj1@gcc.edu>
2015-10-14 13:09:44 -0400

    Added health to HUD and it looks like it works fairly well.

--- Game/Assets/Scripts/cooldownView.cs
+++ Game/Assets/Scripts/cooldownView.cs
@@ -5,10 +5,28 @@ using UnityEngine.UI;
 public class cooldownView : MonoBehaviour {
     public GameObject x, square, circle, triangle;
     public GameObject player;
-	// Use this for initialization
-	void Start () {
-	    
-	}
+    public GameObject HUD;
+    public GameObject healthBar;
+    // Use this for initialization
+
+    private Texture2D healthFront;
+    private Texture2D healthBack;
+    private GUIStyle RectStyle;
+    private float healthBarOriginalPos;
+    private int previousHealth;
+    public float startingHealthPos;
+    void Start () {
+        healthFront = new Texture2D(1, 1);
+        healthBack = new Texture2D(1, 1);
+        RectStyle = new GUIStyle();
+        healthFront.SetPixel(0, 0, Color.green);
+        healthFront.Apply();
+        healthBack.SetPixel(0, 0, Color.red);
+        healthBack.Apply();
+        healthBarOriginalPos = healthBar.transform.position.x;
+        previousHealth = player.GetComponent<Health>().health;
+        startingHealthPos = healthBar.transform.position.x;
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -22,15 +40,23 @@ public class cooldownView : MonoBehaviour {
             {
                 c.a = 1.0f;
             }
-        //Rect rect = new Rect(screenPosition.x - xOffset, screenPosition.y - yOffset, 80, 5);
-        //RectStyle.normal.background = healthBack;
-        //GUI.Box(rect, GUIContent.none, RectStyle);
+            tImage.color = c;
+        //healthBar.transform.Translate(new Vector3(-0.5f, 0, 0));
+        //(RectTransform)healthBar.transform).GetComponent<RectTransform>().rect.width *
 
-        //rect = new Rect(screenPosition.x - xOffset, screenPosition.y - yOffset, 80 * ((float)health / (float)maxHealth), 5);
-        //RectStyle.normal.background = healthFront;
-        //GUI.Box(rect, GUIContent.none, RectStyle);
+        if (previousHealth != player.GetComponent<Health>().health && previousHealth != 0)
+            {
+                //healthBar.transform.Translate(new Vector3(healthBarOriginalPos - healthBar.transform.position.x, 0, 0));
+                healthBar.transform.Translate(new Vector3(((float)player.GetComponent<Health>().health - previousHealth) * ((RectTransform)healthBar.transform).GetComponent<RectTransform>().rect.width 
+                    / (float)player.GetComponent<Health>().maxHealth, 0, 0));
+                if(player.GetComponent<Health>().health <= 0)
+                {
+                    healthBar.transform.Translate(new Vector3(healthBarOriginalPos - healthBar.transform.position.x, 0, 0));
+                }
+            }
 
-        tImage.color = c;
-        ;
-	}
+        previousHealth = player.GetComponent<Health>().health;
+
+    }
+
 }
